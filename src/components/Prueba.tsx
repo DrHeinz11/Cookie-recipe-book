f36551ce6c0e718d455822466a5ba84a23f4e014
Franco Martin <main.francomartin@gmail.com>
2023-06-19 04:29:11 -0300

    Add react-player dependencie

--- src/components/Prueba.tsx
+++ src/components/Prueba.tsx
@@ -1,5 +1,7 @@
+import { Heading, Stack } from "@chakra-ui/react";
 import useFetchData from "../hook/useFetchData";
 import { apiUrls } from "../services";
+import ReactPlayer from "react-player";
 import { Meal } from "../types";
 
 const Prueba = () => {
@@ -17,7 +19,14 @@ const Prueba = () => {
   return (
     <div>
       {data &&
-        data?.meals.map((meal) => <div key={meal.idMeal}>{meal.strMeal}</div>)}
+        data?.meals.map((meal) => {
+          return (
+            <Stack key={meal.idMeal}>
+              <Heading>{meal.strMeal}</Heading>
+              {meal.strYoutube && <ReactPlayer url={meal.strYoutube} controls={true} />}
+            </Stack>
+          );
+        })}
     </div>
   );
 };
